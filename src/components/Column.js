a32afac566aa58f8406ffe6070dff9f9e931b0f3
Victor Johnson <vjohnson@ImageTrend.com>
2018-10-23 13:51:44 -0500

    adjusted the package.json file to differ from the parent project this was forked from

--- src/components/Column.js
+++ src/components/Column.js
@@ -6,9 +6,7 @@ class Column extends Component {
 
     render() {
         const cells = [];
-        for (let i = 0; i < 6; i++) {
-            cells.push(<Cell key={i} />);
-        }
+        for (let i = 0; i < 6; i++) { cells.push(<Cell key={i} />) }
 
         return(
             <div className="column">
