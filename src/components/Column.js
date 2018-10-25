720bf0b2e7ff200ac53e84dc1b325550624336cb
Vic <victordjohnson@gmail.com>
2018-10-24 21:07:58 -0500

    highlight glow for peices only lands on the lowest (index-wise) peice that is not open

--- src/components/Column.js
+++ src/components/Column.js
@@ -11,10 +11,13 @@ class Column extends Component {
     };
 
     render() {
+        let lowestFreeCell = ([...this.props.columnValues].findIndex(cellValue => cellValue !== 0) -1);
+        lowestFreeCell = lowestFreeCell === -2 ? 5: lowestFreeCell;
+
         const cells = this.props.columnValues.map((cellValue, index) =>
-            <Cell key={index} cellValue={cellValue} cellSelection={this.props.cellSelection} columnIndex={this.props.columnIndex} rowIndex={index} /> 
+            <Cell key={index} cellValue={cellValue} cellSelection={this.props.cellSelection} columnIndex={this.props.columnIndex} rowIndex={index} lowestFreeCell={lowestFreeCell === index ? true : false} /> 
         );
-        
+
         return(
             <div className="column">
                 {cells}
