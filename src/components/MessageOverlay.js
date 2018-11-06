dab95008eeeac8ddbe6a3e754b30bf74a99df3c4
Vic <victordjohnson@gmail.com>
2018-11-06 16:39:31 -0600

    message overlay is where i want it to be now

--- src/components/MessageOverlay.js
+++ src/components/MessageOverlay.js
@@ -1,3 +1,4 @@
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import React from 'react';
 
 class MessageOverlay extends React.Component {
@@ -6,21 +7,21 @@ class MessageOverlay extends React.Component {
 
         const overlay =
             this.props.showOverlay 
-            ? (<div className='message-overlay'>
-                <div className='message-container'>
-                    <div className='message-top-half'>
+            ? (<div key={1} className='message-container'>
+                <div className='message-top-half'>
                     <h2>{`Player ${this.props.winningPlayer} Wins!`}</h2>
-                    </div>
-                    <div className='message-bottom-half'>
-                        <button className='message-button' onClick={() => this.props.resetGame()}>Reset</button>
-                    </div>
                 </div>
-            </div>)
+                <div className='message-bottom-half'>
+                    <button className='message-button' onClick={() => this.props.resetGame()}>Reset</button>
+                </div>
+                </div>)
             :(null)
 
         return (
-            <div>
-                {overlay}
+            <div className={`message-overlay ${this.props.showOverlay ? 'message-overlay-on' : ''}`}>
+                <ReactCSSTransitionGroup transitionName="side-slide" transitionEnterTimeout={500} transitionLeaveTimeout={200} >
+                    {overlay}
+                </ReactCSSTransitionGroup>
             </div>
         );
     }
