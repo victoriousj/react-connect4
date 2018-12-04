550d03cf992545ae7959da0dbdea8adbd4abf0b3
victoriousj <vjohnson@ImageTrend.com>
2018-12-04 13:48:11 -0600

    going back into this project to touch-up what I have learned about React

--- src/App.js
+++ src/App.js
@@ -1,88 +1,103 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import React from 'react';
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import React from "react";
 
-import Column from './components/Column';
-import Container from './components/Container';
-import PlayClock from './components/PlayClock';
-import MessageOverlay from './components/MessageOverlay';
+import Column from "./components/Column";
+import Container from "./components/Container";
+import PlayClock from "./components/PlayClock";
+import MessageOverlay from "./components/MessageOverlay";
 
-import * as Helpers from './helpers/helpers';
-import * as GameInteractionCreators from './actions/gameInteractions';
-
+import * as Helpers from "./helpers/helpers";
+import * as GameInteractionCreators from "./actions/gameInteractions";
 
 class App extends React.Component {
-    static propTypes = {
-        isPlaying: PropTypes.bool.isRequired,
-        gameBoard: PropTypes.array.isRequired,
-        winningPieces: PropTypes.array.isRequired,
-        currentPlayer: PropTypes.number.isRequired,
-    };
+  static propTypes = {
+    isPlaying: PropTypes.bool.isRequired,
+    gameBoard: PropTypes.array.isRequired,
+    winningPieces: PropTypes.array.isRequired,
+    currentPlayer: PropTypes.number.isRequired
+  };
 
-    constructor(props) {
-        super(props);
-        
-        const { dispatch } = props;
-        this.dispatch = dispatch;
+  constructor(props) {
+    super(props);
 
-        this.endGame = bindActionCreators(GameInteractionCreators.endGame, dispatch);
-        this.incTimer = bindActionCreators(GameInteractionCreators.incTimer, dispatch);
-        this.resetGame = bindActionCreators(GameInteractionCreators.resetGame, dispatch);
-        this.cellSelection = bindActionCreators(GameInteractionCreators.cellSelection, dispatch);
-    };
+    const { dispatch } = props;
+    this.dispatch = dispatch;
 
-    componentDidMount() {
-        setInterval(this.incTimer, 1000);
-    }
+    this.endGame = bindActionCreators(
+      GameInteractionCreators.endGame,
+      dispatch
+    );
+    this.incTimer = bindActionCreators(
+      GameInteractionCreators.incTimer,
+      dispatch
+    );
+    this.resetGame = bindActionCreators(
+      GameInteractionCreators.resetGame,
+      dispatch
+    );
+    this.cellSelection = bindActionCreators(
+      GameInteractionCreators.cellSelection,
+      dispatch
+    );
+  }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.isPlaying) {
-            let winningPieces = Helpers.checkGameBoard(this.props.gameBoard);
-            if (winningPieces) {
-                console.log(winningPieces);
-                this.endGame();
-                this.dispatch(GameInteractionCreators.registerGameWinningPeices(winningPieces));
-            }
-        }
-    }
-    
-    render() { 
-        let columns = this.props.gameBoard.map((columnValues, index) =>
-            <Column 
-                key=            {index} 
-                columnIndex=    {index} 
-                columnValues=   {columnValues} 
-                cellSelection=  {this.cellSelection} 
-                isPlaying=      {this.props.isPlaying} 
-                currentPlayer=  {this.props.currentPlayer} 
-                winningPieces=  {this.props.winningPieces}/>
+  componentDidMount() {
+    setInterval(this.incTimer, 1000);
+  }
+
+  componentDidUpdate() {
+    if (this.props.isPlaying) {
+      let winningPieces = Helpers.checkGameBoard(this.props.gameBoard);
+      if (winningPieces) {
+        console.log(winningPieces);
+        this.endGame();
+        this.dispatch(
+          GameInteractionCreators.registerGameWinningPeices(winningPieces)
         );
-
-        
-        return (
-            <div className = "App">
-                <MessageOverlay winningPlayer={this.props.currentPlayer === 1 ? 2: 1} showOverlay={this.props.showOverlay} resetGame={this.resetGame} />
-                <div className="playclocks">
-                    <PlayClock player={1} time={this.props.playerOneTime} />
-                    <PlayClock player={2} time={this.props.playerTwoTime} />
-                </div>
-                <Container Columns={columns} />
-            </div>
-        );
+      }
     }
+  }
+
+  render() {
+    let columns = this.props.gameBoard.map((columnValues, index) => (
+      <Column
+        key={index}
+        columnIndex={index}
+        columnValues={columnValues}
+        cellSelection={this.cellSelection}
+        isPlaying={this.props.isPlaying}
+        currentPlayer={this.props.currentPlayer}
+        winningPieces={this.props.winningPieces}
+      />
+    ));
+
+    return (
+      <div className="App">
+        <MessageOverlay
+          winningPlayer={this.props.currentPlayer === 1 ? 2 : 1}
+          showOverlay={this.props.showOverlay}
+          resetGame={this.resetGame}
+        />
+        <div className="playclocks">
+          <PlayClock player={1} time={this.props.playerOneTime} />
+          <PlayClock player={2} time={this.props.playerTwoTime} />
+        </div>
+        <Container Columns={columns} />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => (
-    {
-        gameBoard: state.gameBoard,
-        isPlaying: state.isPlaying,
-        showOverlay: state.showOverlay,
-        currentPlayer: state.currentPlayer,
-        winningPieces: state.winningPieces,
-        playerOneTime: state.playerOneTime,
-        playerTwoTime: state.playerTwoTime,
-    }
-);
+const mapStateToProps = state => ({
+  gameBoard: state.gameBoard,
+  isPlaying: state.isPlaying,
+  showOverlay: state.showOverlay,
+  currentPlayer: state.currentPlayer,
+  winningPieces: state.winningPieces,
+  playerOneTime: state.playerOneTime,
+  playerTwoTime: state.playerTwoTime
+});
 
 export default connect(mapStateToProps)(App);
